ec30c9688b24910fc8e9250bb9c30f6a42a99dcf
Marcelo Almeida Hirota <marcelohirota@gmail.com>
2020-06-02 13:42:36 +0930

    algorithms='HS256' encryptation

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import jwt
 
 
 def create_token(data, secret):
-    return jwt.encode(payload=data, key=secret)
+    return jwt.encode(payload=data, key=secret, algorithm='HS256')
 
 
 def verify_signature(token):
     try:
-        return jwt.decode(jwt=token, key='acelera')
+        return jwt.decode(jwt=token, key='acelera', algorithms='HS256')
 
     except:
         return {"error": 2}
+
+
+token = create_token({"language": "Python"}, 'acelera')
+
+print(token)
